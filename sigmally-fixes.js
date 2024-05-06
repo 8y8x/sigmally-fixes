c4739dec672cdfeb976f474b4481ab30592af676
8y8x <61947825+8y8x@users.noreply.github.com>
2024-05-05 21:45:25 -0700

    Bump version to 2.2.7

--- sigmally-fixes.js
+++ sigmally-fixes.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Sigmally Fixes V2
-// @version      2.2.6
+// @version      2.2.7
 // @description  Easily 3X your FPS on Sigmally.com + many bug fixes + great for multiboxing + supports SigMod
 // @author       8y8x
 // @match        https://*.sigmally.com/*
