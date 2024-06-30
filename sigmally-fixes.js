8c1fe181ec9a0562f82f7d9a28b6f2fc415ba318
8y8x <61947825+8y8x@users.noreply.github.com>
2024-06-30 11:25:33 -0700

    Bump version to 2.3.0

--- sigmally-fixes.js
+++ sigmally-fixes.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Sigmally Fixes V2
-// @version      2.2.10
+// @version      2.3.0
 // @description  Easily 3X your FPS on Sigmally.com + many bug fixes + great for multiboxing + supports SigMod
 // @author       8y8x
 // @match        https://*.sigmally.com/*
@@ -357,7 +357,7 @@
 
 			const watermark = document.createElement('span');
 			watermark.innerHTML = '<a href="https://greasyfork.org/scripts/483587/versions" \
-				target="_blank">Sigmally Fixes 2.2.9</a> by yx';
+				target="_blank">Sigmally Fixes 2.3.0</a> by yx';
 			title.insertAdjacentElement('afterend', watermark);
 		})();
 
