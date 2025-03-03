057b8e703c140c128cbad2cd7f8406a02e183948
yx <61947825+8y8x@users.noreply.github.com>
2025-03-03 10:55:04 -0800

    remove cell interpolation setting

--- sigmally-fixes.user.js
+++ sigmally-fixes.user.js
@@ -1190,7 +1190,6 @@
 			clanScaleFactor: 1,
 			colorUnderSkin: true,
 			drawDelay: 120,
-			interpolation: 'linear',
 			jellySkinLag: true,
 			massBold: false,
 			massOpacity: 1,
@@ -1608,10 +1607,6 @@
 		setting('Draw delay', [slider('drawDelay', 120, 40, 300, 1, 0)], () => true,
 			'How long (in milliseconds) cells will lag behind for. Lower values mean cells will very quickly catch ' +
 			'up to where they actually are.');
-		setting(`Cell movement ${newTag}`, [dropdown('interpolation', [['exp', 'Exponential (smoother, like Delta)'],
-			['linear', 'Linear (default)']])], () => true,
-		'Some players feel that their game is smoother when using Delta. If you feel that way, try using the ' +
-			'"Exponential" setting.');
 		setting('Cell outlines', [checkbox('cellOutlines')], () => true,
 			'Whether the subtle dark outlines around cells (including skins) should draw.');
 		setting('Cell opacity', [slider('cellOpacity', 1, 0, 1, 0.005, 3)], () => true,
@@ -2137,24 +2132,14 @@
 				ny = killer.ny;
 			}
 
-			const dt = (now - cell.updated) / 1000;
+			let a = (now - cell.updated) / settings.drawDelay;
+			a = a < 0 ? 0 : a > 1 ? 1 : a;
 
 			let x, y, r;
 			if (cell.pellet && cell.deadAt === undefined) {
 				x = nx;
 				y = ny;
 				r = cell.nr;
-			} else if (settings.interpolation === 'exp') {
-				// delta uses """linear interpolation""", except .ox, .oy, and .or are always updated every frame.
-				// at infinite FPS, this is purely exponential (the speed a cell moves at is proportional to itself).
-
-				// the factor is derived from f(1/60) = g(1/60) (so it looks identical to Delta at 60FPS) where:
-				// linear interpolation, f(t) = (1000 / dd) * t, which is basically the variable a
-				// exponential interpolation, g(t) = aux.exponentialEase(factor, t), where o=0 and n=1
-				const factor = 60 / 1000 * settings.drawDelay;
-				x = aux.exponentialEase(cell.ox, nx, factor, dt);
-				y = aux.exponentialEase(cell.oy, ny, factor, dt);
-				r = aux.exponentialEase(cell.or, cell.nr, factor, dt);
 			} else {
 				let a = (now - cell.updated) / settings.drawDelay;
 				a = a < 0 ? 0 : a > 1 ? 1 : a;
@@ -2163,6 +2148,8 @@
 				y = cell.oy + (ny - cell.oy) * a;
 				r = cell.or + (cell.nr - cell.or) * a;
 			}
+
+			const dt = (now - cell.updated) / 1000;
 
 			return {
 				x, y, r,
